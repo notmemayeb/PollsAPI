755d2c9760db342843a31050d95c3d7f73bac3e3
Notmemayeb <notmemaybeatwork@gmail.com>
2022-12-02 21:56:13 +0100

    ChoiceList and CreateVote added

--- PollsAPI/polls/urls.py
+++ PollsAPI/polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 
 urlpatterns = [
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
     path("polls/", PollList.as_view(), name='polls_list'),
     path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail')
 ]
