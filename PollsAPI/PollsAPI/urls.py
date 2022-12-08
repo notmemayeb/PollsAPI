68c67bddee3d09efdc4b7723c5aa7d5f87d4ea46
Notmemayeb <notmemaybeatwork@gmail.com>
2022-12-08 13:36:04 +0100

    CoreAPI docs added

--- PollsAPI/PollsAPI/urls.py
+++ PollsAPI/PollsAPI/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
 
 schema_view = get_swagger_view(title='Polls API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('polls.urls')),
-    path(r'swagger-docs/', schema_view)
+    path(r'swagger-docs/', schema_view),
+    path(r'docs/', include_docs_urls(title='Polls API')),
 ]
 
 
